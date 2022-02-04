bbad0c5b76e31ab432069356079c2d1fe8175ed7
Sebastian Achmuller de acosta <sachmull@3-F-9.42heilbronn.de>
2022-02-04 19:57:36 +0100

    Ctrl-C now gets handled correctly

--- src/signal_handling/signal_handling.c
+++ src/signal_handling/signal_handling.c
@@ -6,19 +6,18 @@
 /*   By: sachmull <sachmull@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/04 16:17:25 by sachmull          #+#    #+#             */
-/*   Updated: 2022/02/04 19:24:01 by sachmull         ###   ########.fr       */
+/*   Updated: 2022/02/04 19:56:42 by sachmull         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <signal_handling.h>
 #include <stdlib.h>
+#include <termios.h>
 
 static void	handle_signal_int(int sig)
 {
 	if (sig == SIGINT)
 	{
-		printf("\033[K");
-		printf("> ");
 		printf("\n");
 		rl_on_new_line();
 		rl_replace_line("", 0);
@@ -28,6 +27,11 @@ static void	handle_signal_int(int sig)
 
 void	handle_signals()
 {
+	struct termios	terminal;
+
+	tcgetattr(2, &terminal);
+	terminal.c_lflag = terminal.c_lflag ^ ECHOCTL;
+	tcsetattr(2, TCSANOW, &terminal);
 	signal(SIGINT, handle_signal_int);
 	signal(SIGQUIT, SIG_IGN);
 }
