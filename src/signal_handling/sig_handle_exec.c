72c4a5e673d879ded4ecb920dbaea9ef7d6ea9f3
Sebastian Achmuller de acosta <sachmull@3-F-9.42heilbronn.de>
2022-02-24 16:02:22 +0100

    Fixed signal handling during exec: print ctrl chars

--- src/signal_handling/sig_handle_exec.c
+++ src/signal_handling/sig_handle_exec.c
@@ -6,7 +6,7 @@
 /*   By: sachmull <sachmull@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/08 19:06:27 by sachmull          #+#    #+#             */
-/*   Updated: 2022/02/13 18:13:05 by sachmull         ###   ########.fr       */
+/*   Updated: 2022/02/24 16:00:53 by sachmull         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,12 @@ static void	handle_sigquit(int sig)
 
 void	sig_handle_exec(void)
 {
+	struct termios	terminal;
+
+	tcgetattr(2, &terminal);
+	if (!(terminal.c_lflag & ECHOCTL))
+		terminal.c_lflag = terminal.c_lflag ^ ECHOCTL;
+	tcsetattr(2, TCSANOW, &terminal);
 	signal(SIGINT, handle_sigint);
 	signal(SIGQUIT, handle_sigquit);
 	signal(SIGSEGV, exit);
